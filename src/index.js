f63fc9ebd8f54810236d637d77ef2b72ca29a676
Rishabh Prasanna <88023315+rish490@users.noreply.github.com>
2021-10-09 20:08:10 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//this is the completed version of todolist app project
+//made only by using things we have already seen
